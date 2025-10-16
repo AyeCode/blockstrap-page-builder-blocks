9d3f7861e41585ef2e448c4db08c0f238336cdb1
Kiran Prajapati <officeofkiran@gmail.com>
2025-10-16 19:58:24 +0530

    0.1.44

--- blockstrap-page-builder-blocks.php
+++ blockstrap-page-builder-blocks.php
@@ -11,7 +11,7 @@
  * Plugin Name: BlockStrap Page Builder Blocks
  * Plugin URI: https://ayecode.io/
  * Description: BlockStrap - A FSE page builder for WordPress
- * Version: 0.1.43
+ * Version: 0.1.44
  * Author: AyeCode
  * Author URI: https://ayecode.io
  * Text Domain: blockstrap-page-builder-blocks
@@ -21,7 +21,7 @@
  */
 
 
-define( 'BLOCKSTRAP_BLOCKS_VERSION', '0.1.43' );
+define( 'BLOCKSTRAP_BLOCKS_VERSION', '0.1.44' );
 
 /**
  * The BlockStrap Class
